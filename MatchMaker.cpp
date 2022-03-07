d291bc88d3fb590674f19fc0c8349e1b51dc72e6
ehng359 <94334288+ehng359@users.noreply.github.com>
2022-03-06 18:58:58 -0800

    Add files via upload

--- MatchMaker.cpp
+++ MatchMaker.cpp
@@ -1,4 +1,4 @@
-#include <unordered_set>
+#include <set>
 #include "MatchMaker.h"
 MatchMaker::MatchMaker(const MemberDatabase& mdb, const AttributeTranslator& at) {
 	this->mdb = &mdb;
@@ -9,27 +9,27 @@ MatchMaker::~MatchMaker() {}
 
 std::vector<EmailCount> MatchMaker::IdentifyRankedMatches(std::string email, int threshold) const {
 	int total = 0;
-	std::vector<EmailCount> total;
-	std::unordered_set<AttValPair> uniqueAttVals;
-	std::unordered_set<AttValPair> uniqueCompatAttVals;
+	std::vector<EmailCount> eCount;
+	std::vector<AttValPair> uniqueAttVals;
 	std::vector<AttValPair> compatPairs;
 	const PersonProfile* p = mdb->GetMemberByEmail(email);
 	AttValPair avp;
 	for (int i = 0; i < p->GetNumAttValPairs(); i++) {
 		p->GetAttVal(i, avp);
-		uniqueAttVals.insert(avp);
+		if (!uniqueAttVals.)
+			uniqueAttVals.push_back(avp);
 		compatPairs = at->FindCompatibleAttValPairs(avp);
-		for (int j = 0; j < compatPairs.size(); j++)
-			uniqueCompatAttVals.insert(compatPairs[j]);
+		for (int j = 0; j != compatPairs.size(); j++)
+			uniqueAttVals.push_back(compatPairs[j]);
 	}
-	for (std::unordered_set<AttValPair>::iterator it = uniqueAttVals.begin(), ct = uniqueCompatAttVals.begin(); it != uniqueAttVals.end() && ct != uniqueCompatAttVals.end();) {
+	for (std::vector<AttValPair>::iterator it = uniqueAttVals.begin(); it != uniqueAttVals.end();) {
 		if (it != uniqueAttVals.end()) {
 			total += mdb->FindingMatchingMembers(*it).size();
 			it++;
 		}
-		else if (ct != uniqueCompatAttVals.end()) {
-			total += mdb->FindingMatchingMembers(*ct).size();
-			ct++;
+		else {
+			break;
 		}
 	}
+	return eCount;
 }
